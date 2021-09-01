516dba2704da180dc0358dab8ed76724c098da2b
fcadmin <fcadmin@192.168.1.4>
2021-09-02 00:32:55 +0530

    commit

---
+++ Leetcode/src/home/tech/problem/MyLinkedNode.java
@@ -0,0 +1,60 @@
+package home.tech.problem;
+
+public class MyLinkedNode {
+
+	protected static ListNode head = null;
+	
+	public MyLinkedNode(Integer... values) {
+		head = build(values,0,values.length);
+	}
+
+	public static void main(String[] args) {
+		MyLinkedNode obj = new MyLinkedNode(1,2,3,4,5);
+		obj.printListNode();
+		obj.get();
+	}
+
+	protected ListNode get() {
+		return head;
+		
+	}
+
+	private static ListNode build(Integer[] values, int pos, int length) {
+		if(pos == values.length) {
+			return null;
+		}
+		ListNode nextNode = build(values,pos+1,length);
+		ListNode node = new ListNode();
+		node.val = values[pos];
+		node.next = nextNode;
+		return node;		
+	}
+	
+	protected void printListNode() {
+		ListNode temp = head;
+		System.out.print("[ ");
+		while(temp != null) {
+			System.out.print(temp.val+" ");
+			temp = temp.next;
+		}
+		System.out.print("]");
+		System.out.println("");
+	}
+	
+	protected void printListNodeExternal(ListNode head) {
+		ListNode temp = head;
+		System.out.print("[ ");
+		while(temp != null) {
+			System.out.print(temp.val+" ");
+			temp = temp.next;
+		}
+		System.out.print("]");
+		System.out.println("");
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ Leetcode/src/home/tech/problem/ReverseLinkedList.java
@@ -0,0 +1,30 @@
+package home.tech.problem;
+
+public class ReverseLinkedList {
+
+	public static void main(String[] args) {
+		MyLinkedNode listNode = new MyLinkedNode(1,2,3,4);
+		ListNode head = listNode.get();
+		
+		ReverseLinkedList obj = new ReverseLinkedList();
+		listNode.printListNodeExternal(obj.reverseList(head));	
+
+	}
+	
+	public ListNode reverseList(ListNode head) {
+        
+	    ListNode prev = null;
+	    ListNode curr = head;
+	    ListNode next = null;
+	    
+	    while(curr.next != null) {
+	    	next = curr.next;
+	    	curr.next = prev;
+	    	prev = curr;
+	    	curr = next;
+	    }
+	    curr.next = prev;
+	    return curr;
+	}
+
+}
